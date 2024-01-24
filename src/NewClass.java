ad5c1d2ce0a3745a417741f1e144a78c288a5e69
Nurzat <bekeshevnurzat@gmail.com>
2024-01-24 10:27:57 +0600

    last commit again

---
+++ src/NewClass.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+    }
+}
