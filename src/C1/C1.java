474a09906770441276c95fb68fe8c8405f961596
Nurzat <bekeshevnurzat@gmail.com>
2024-01-25 10:27:10 +0600

    hello1

--- src/C1/C1.java
+++ src/C1/C1.java
@@ -2,7 +2,6 @@ package C1;
 
 public class C1 {
     public static void main(String[] args) {
-        System.out.println("Изменения");
-        System.out.println("New Update");
+        System.out.println("123");
     }
 }
